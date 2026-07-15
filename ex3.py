eb08e121ae86fe1fb282cce21448f1b4788f80a0
aayushjoshi992 <aayushj992@gmail.com>
2026-07-15 12:41:46 +0545

    dictionary learning

--- ex3.py
+++ ex3.py
@@ -22,12 +22,37 @@
 # del d["company"]
 # print(d)
 
-dt= dict([("name","ritu"),("id",10),("subject","python")])
+# dt= dict([("name","ritu"),("id",10),("subject","python")])
 
-for k in dt.keys():
-    if k=="id":
-        continue
-    print(k)
-d3=dict(Name="Dipika", Age=6)
-print(d3) 
+# for k in dt.keys():
+#     if k=="id":
+#         continue
+#     print(k)
+# d3=dict(Name="Dipika", Age=6)
+# print(d3) 
 
+
+# d={}
+# name=input("Enter name:")
+# course=input("Enter course:")
+# fee=int(input("Enter fee:"))
+# d["name"]=name
+# d["course"]=course
+# d["fee"]=fee
+# print(d)
+
+
+
+def check(li,di):
+    sum=0
+    if type(li)==list:
+        for l in li:
+            sum=sum+l
+        print(sum)
+    if type(di)==dict:
+        print(di)
+
+
+di={"abc":1,"cde":2}
+li=[1,2,3,4,5]
+check(li,di)
