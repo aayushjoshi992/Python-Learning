cb6f48b5cdc66fbfebbf4babe7684158c5fb1c49
aayushjoshi992 <aayushj992@gmail.com>
2026-08-06 18:04:39 +0545

    learning file handling

--- f3.py
+++ f3.py
@@ -1,3 +1,9 @@
 ap=open("data.txt",'a')
 ap.write('\nNear the office we have bagmati bridge')
 ap.close()
+
+import os
+if os.path.exists("abc.txt"):
+    os.remove("abc.txt")
+else:
+    print("The file does not exist")
