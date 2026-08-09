9212c2043b760cdb118864c36457b0206c97363d
aayushjoshi992 <aayushj992@gmail.com>
2026-08-09 18:02:24 +0545

    completed numpy task

---
+++ task.py
@@ -0,0 +1,11 @@
+import numpy as np
+a = np.array([2,3,4,5,6,8,10])
+m=np.max(a)
+print("Max ",m)
+mi=np.min(a)
+print("Min ",mi)
+print("length ",len(a))
+
+for i in a:
+    if(i%2==0):
+        print(i)
