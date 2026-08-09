fc0c1888922b40ddfa529b35fdcaeae01c95550e
aayushjoshi992 <aayushj992@gmail.com>
2026-08-09 17:33:23 +0545

    learning more with num py

--- np.py
+++ np.py
@@ -2,11 +2,28 @@
 # Numerical computation use garna
 
 import numpy as np
-ar=np.array([1,2,3,4,5])
+# ar=np.array([1,2,3,4,5])
 # print(ar)
 # print(type(ar))
 # print(ar.ndim)
 # print(ar[3])
 # print(len(ar))
-for a in ar:
-    print(a)
+# for a in ar:
+#     print(a)
+
+# arr=np.array([[1,2,3],[4,5,6]])
+# print(arr)
+
+# print(arr.ndim)
+
+#We can also make Array using the range function
+
+# arr=np.arange(0,10,2)
+# print(arr)
+
+# print(arr.shape)
+# print(arr.shape)
+
+a=np.array([10,20,30])
+b=np.array([1,2,3])
+print(a+b)
