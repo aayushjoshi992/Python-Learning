de60448f8eac14684b0b85625b0b15e897775899
aayushjoshi992 <aayushj992@gmail.com>
2026-08-09 17:52:34 +0545

    learned num py

--- np.py
+++ np.py
@@ -1,35 +1,35 @@
-#Numpy
+# Numpy
 # Numerical computation use garna
 
 import numpy as np
-# ar=np.array([1,2,3,4,5])
-# print(ar)
-# print(type(ar))
-# print(ar.ndim)
-# print(ar[3])
-# print(len(ar))
-# for a in ar:
-#     print(a)
+ar=np.array([1,2,3,4,5])
+print(ar)
+print(type(ar))
+print(ar.ndim)
+print(ar[3])
+print(len(ar))
+for a in ar:
+    print(a)
 
-# arr=np.array([[1,2,3],[4,5,6]])
-# print(arr)
+arr=np.array([[1,2,3],[4,5,6]])
+print(arr)
 
-# print(arr.ndim)
+print(arr.ndim)
 
-#We can also make Array using the range function
+# We can also make Array using the range function
 
-# arr=np.arange(0,10,2)
-# print(arr)
+arr=np.arange(0,10,2)
+print(arr)
 
-# print(arr.shape)
-# print(arr.shape)
+print(arr.shape)
+print(arr.shape)
 
 a=np.array([10,20,30])
 b=np.array([1,2,3])
-# print(a+b)
-# print(a-b)
+print(a+b)
+print(a-b)
 
-# print(a+5)
+print(a+5)
 
 print(a*2)#broadcasting
 print(np.sqrt(a))
@@ -43,3 +43,14 @@ minimum=np.min(a)
 print(add)
 print(maximum)
 print(minimum)
+
+m=np.mean(a)
+print(m)
+
+
+print(np.std(a))
+
+
+a=np.array([[1,2],[3,4]])
+b=np.array([[5,6],[7,8]])
+print(a+b)
