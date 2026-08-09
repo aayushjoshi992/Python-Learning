ae35a8d186feb4f1f46ac7e10f3ee542f4631438
aayushjoshi992 <aayushj992@gmail.com>
2026-08-09 17:24:36 +0545

    learning num py

---
+++ np.py
@@ -0,0 +1,12 @@
+#Numpy
+# Numerical computation use garna
+
+import numpy as np
+ar=np.array([1,2,3,4,5])
+# print(ar)
+# print(type(ar))
+# print(ar.ndim)
+# print(ar[3])
+# print(len(ar))
+for a in ar:
+    print(a)
