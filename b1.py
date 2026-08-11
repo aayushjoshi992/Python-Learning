a871cb52993f2397a1dc4cc717b6eacada90cbc8
aayushjoshi992 <aayushj992@gmail.com>
2026-08-11 18:18:57 +0545

    leared vectorization

--- b1.py
+++ b1.py
@@ -10,10 +10,32 @@ arr =np.array([1,2,3,4])
 # [1 2 3 4]
 # +
 # [10 10 10 10]
-a=np.array([[1,2,3],
-            [4,5,6]])
+# a=np.array([[1,2,3],
+#             [4,5,6]])
 # b=np.array([10,20,30])
 
 
-b=np.array([[100],[200]])
-print(a+b)
+# b=np.array([[100],[200]])
+# print(a+b)
+a=np.array([1,2,3,4,5])
+result=[]
+for i in a:
+    result.append(i*3)
+# print(result)
+res=a*3
+# print(res)
+a=np.array([10,20,30,40])
+# print(a>25)
+# print((a>15) & (a<35))
+a=np.array([10,20,30,40])
+result=np.where(a>25,'High','Low')
+print(result)
+
+# print(np.max(a))
+# print(np.min(a))
+# print(np.sum(a))
+# print(np.sqrt(a))
+# print(np.exp(a))
+# print(np.log(a))
+arr=np.array([1,2,3,4])
+print(arr!=3)
