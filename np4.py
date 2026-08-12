228490d99e5baa82536fb1ab1f040f3280dae91f
aayushjoshi992 <aayushj992@gmail.com>
2026-08-12 17:31:07 +0545

    Learning more numpy

---
+++ np4.py
@@ -0,0 +1,33 @@
+# Mathematical and Statistical Operation in Numpy
+import numpy as np
+
+#Basic Mathematical Operations
+arr= np.array([10,20,30,40,50])
+print(arr)
+print(np.add(arr,10))
+print(np.subtract(arr,5))
+print(np.multiply(arr,2))
+print(np.divide(arr,2).astype(int))
+print(np.pow(arr,2))
+print(np.sqrt(arr))
+print(np.exp(arr))
+print(np.log(arr))
+arr=np.array([-2,3,-4,25])
+print(np.abs(arr))
+val=3.333333333
+print(f" The value is {val:.2f}")
+prices=np.array([10.40,20.60,31.90,55.20])
+print(np.round(prices).astype(int))
+angles=np.array([0,np.pi/2,np.pi])
+angles=np.array([0,90,180,270,360])
+print(angles)
+print(f"sin functio:{np.sin(angles)}")
+print(f"cos func: {np.cos(angles)}")
+print(f"tan func: {np.tan(angles)}")
+
+# Statistical operation
+print(np.std(arr))
+print(np.var(arr))
+print(np.max(arr))
+print(np.min(arr))
+
