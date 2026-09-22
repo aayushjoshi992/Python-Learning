e591a81f2dc1a25e9f5c6f5d2530dd4be6432cb7
aayushjoshi992 <aayushj992@gmail.com>
2026-09-22 19:50:36 +0545

    learned random forest and decision tree

--- np2.py
+++ np2.py
@@ -3,23 +3,23 @@ import numpy as np
 # arr=np.array([10,20,30,40,50])
 # print(arr)
 # print(arr[3])
-# arr2=np.array([[1,2,3],
-#                [4,5,6],
-#                [7,8,9]])
+arr2=np.array([[1,2,3],
+               [4,5,6],
+               [7,8,9]])
 # print(arr2)
 # print(arr2[1][1])
 # print(arr2[2][2])
 
 # arr=np.array([10,20,30,40,50])
+# print(arr[::-1])
 # print(arr[1:4])
 # print(arr[2:])
-# print(arr[::-1])
 # print(arr[-2])
 
 # arr2=np.array([[1,2,3],
-            #    [4,5,6],
-            #    [7,8,9]])
-# print(arr2)
+#                [4,5,6],
+#                [7,8,9]])
+
 
 # print(arr2[:2, 0:2])
 
@@ -28,7 +28,7 @@ import numpy as np
 # print(arr2[2,:])
 
 # arr=np.array([1,2,3,4,5,6])
-# rs=arr.reshape(3,2)
+# rs=arr.reshape(2,3)
 # print(rs)
 # print(rs.ndim)
 
@@ -37,14 +37,14 @@ import numpy as np
 # print(arr_2d.ndim)
 
 # arr_3d=arr_2d.reshape(1,3,2)
-# print(arr_3d.ndim)
+# print(arr_3d)
 
 # arr_3d=arr_2d.reshape(1,2,3)
 # print(arr_3d)
 
 
 ar2=np.arange(24).reshape(3,4,2)
-# print(ar2)
+print(ar2)
 
 ar3=np.arange(24).reshape(6,4)
 print(ar3)
